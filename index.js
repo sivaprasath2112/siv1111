3e43fcfd46f7dd8a71940dcd89c57f7c4cf14d6e
sivaprasath2112 <sivaprasath.s.r@gmail.com>
2023-02-04 09:30:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.use(express())
+app.get('/',(req,res)=>{
+    res.send('Hello from nodejs')
+})
+const PORT = process.env.PORT || 8000
+app.listen(PORT,()=>{
+console.log(`server is running on port ${PORT}`)
+})
